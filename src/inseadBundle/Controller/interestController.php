184af693721ac3cfcbe164b3bd3d6f5977cb8707
SebiLob <solomon.grundy.51@gmail.com>
2017-05-23 23:05:07 +0200

    vews 8/10

--- src/inseadBundle/Controller/interestController.php
+++ src/inseadBundle/Controller/interestController.php
@@ -4,11 +4,11 @@ namespace inseadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class menuController extends Controller
+class interestController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@insead/user/menu.html.twig', array(
+        return $this->render('@insead/user/interest.html.twig', array(
             // ...
         ));
     }
--- src/inseadBundle/Resources/public/js/insead.js
+++ src/inseadBundle/Resources/public/js/insead.js
@@ -8,3 +8,10 @@ $(".button-collapse").sideNav();
 $(document).ready(function(){
     $('.parallax').parallax();
 });
+
+/*DatePicker*/
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
