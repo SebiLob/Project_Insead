9b19d04eb4d47bfbd64c6fc4a6887b092193d9fe
SebiLob <solomon.grundy.51@gmail.com>
2017-05-24 10:41:27 +0200

    save

--- src/inseadBundle/Controller/PlanTripController.php
+++ src/inseadBundle/Controller/PlanTripController.php
@@ -3,13 +3,29 @@
 namespace inseadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use inseadBundle\Entity\createTrip;
+use inseadBundle\Form\createTripType;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlanTripController extends Controller
 {
-    public function indexAction()
+    public function addTripAction(Request $request)
     {
+        $trip = new createTrip();
+        $form = $this->createForm(createTripType::class, $trip);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($trip);
+            $em->flush();
+
+            return $this->redirectToRoute('insead_trip');
+        }
         return $this->render('@insead/user/planTrip.html.twig', array(
-            // ...
+            'form' => $form->createView(),
+            'trip' => $trip
         ));
     }
 
